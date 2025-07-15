04d6b71f8b0380a4398aafbfc1bc579604ba9e9a
shahednur <shahednurctg@gmail.com>
2025-07-16 01:40:06 +0600

    add: Inventory entity

---
+++ inventoryservice/src/main/java/com/shahed/inventoryservice/entity/Inventory.java
@@ -0,0 +1,34 @@
+package com.shahed.inventoryservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "inventories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "sku", nullable = false, length = 50)
+    private String sku;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+}
