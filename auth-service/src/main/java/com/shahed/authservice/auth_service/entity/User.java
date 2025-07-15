f26b6c13e4200fcd61380ede67041df7347075b0
shahednur <shahednurctg@gmail.com>
2025-07-15 15:59:55 +0600

    Add User entity with validation and JPA annotations

---
+++ auth-service/src/main/java/com/shahed/authservice/auth_service/entity/User.java
@@ -0,0 +1,48 @@
+package com.shahed.authservice.auth_service.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Email(message = "Email should be valid")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private String role = "USER";
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
