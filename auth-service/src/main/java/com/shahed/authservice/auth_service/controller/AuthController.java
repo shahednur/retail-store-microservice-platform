f7825749e111277a8be01c6ef34a40ee3c13ccba
shahednur <shahednurctg@gmail.com>
2025-08-17 19:05:43 +0600

    Refactored and updated AuthController

--- auth-service/src/main/java/com/shahed/authservice/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/shahed/authservice/auth_service/controller/AuthController.java
@@ -1,28 +1,17 @@
 package com.shahed.authservice.auth_service.controller;
 
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import com.shahed.authservice.auth_service.dto.CreateUserRequest;
-import com.shahed.authservice.auth_service.dto.UserResponse;
 import com.shahed.authservice.auth_service.service.AuthService;
 import com.shahed.authservice.auth_service.util.ApiResponse;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.RequestParam;
