0149505dc00b59be37354f6cd9fcb6a7089ea42d
shahednur <shahednurctg@gmail.com>
2025-08-22 22:57:43 +0600

    Added notification sender service

---
+++ notificationservice/src/main/java/com/shahed/notificationservice/sender/InAppNotificationSender.java
@@ -0,0 +1,25 @@
+package com.shahed.notificationservice.sender;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.shahed.notificationservice.entity.Notification;
+import com.shahed.notificationservice.entity.NotificationStatus;
+import com.shahed.notificationservice.repository.NotificationRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class InAppNotificationSender implements NotificationSender {
+
+    private final NotificationRepository repository;
+
+    @Override
+    public void send(Notification notification) {
+        repository.save(notification);
+        notification.setStatus(NotificationStatus.SENT);
+        notification.setSentAt(LocalDateTime.now());
+    }
+}
